60e4a9e8672a27a777f0b0e31fe49328529a367d
diogoichaso <dichaso@gmail.com>
2025-10-06 21:11:19 -0300

    sumiit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,3 +1,6 @@
+/**Aula 10 - para usar o useState é preciso importar */
+import { useState } from "react"
+
 function Form(){
 
     function cadastrarUsuario(e){
@@ -5,14 +8,40 @@ function Form(){
         
         Normalmente o submit envia a informação e recarrega a página*/
         console.log("Usuário cadastrado")
+        console.log(`O usuário ${name} foi cadastrado com a senha ${pass}`)
     }
+
+    /**o useState é usado em uma constante com a dupla [recuperação do valor, atribuição do valor] é como se fosse 
+     * [variável, função para alterar a variável]*/
+    const [name, setName] = useState('valor default')
+    const [pass, setPass] = useState()
 
     return(
         <>
             <h1>Meu cadastro:</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type = "text" placeholder = "Digite seu nome"></input>
+                    <label htmlFor="name">Nome:</label>
+                    <input 
+                        type = "text" 
+                        id = "name" 
+                        name="name" 
+                        placeholder = "Digite seu nome"
+                        value={name}/**inicializa o valor do input */
+                        onChange={(e) => setName(e.target.value)}
+                        /*a cada letra digitada muda o estado */
+                    />
+
+                </div>
+                 <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input 
+                        type = "password" 
+                        id = "password" 
+                        name = "password" 
+                        placeholder = "Digite sua senha"
+                        onChange={(e) => setPass(e.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value = "Cadastrar"></input>
